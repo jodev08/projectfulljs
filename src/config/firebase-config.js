e6951505e23a640a5ce5c1ddc3aca886cd427ed0
jstonska <j.stonska@apefe.org>
2024-11-19 14:50:47 +0100

    mise en place firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC-3eIpkFTbHujnAih49YgObDG6ugrJAs",
+  authDomain: "projectjavascript-9532d.firebaseapp.com",
+  projectId: "projectjavascript-9532d",
+  storageBucket: "projectjavascript-9532d.firebasestorage.app",
+  messagingSenderId: "304726147536",
+  appId: "1:304726147536:web:645d799a2585db8b1a39d5"
+};
+
+const app = initializeApp(firebaseConfig);
